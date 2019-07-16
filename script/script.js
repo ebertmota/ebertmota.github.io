063ea1f8d00c3ea43b5fe2a30fb8a31b1109d355
ebertmota03 <ebert.mota03@gmail.com>
2019-07-15 21:09:07 -0300

    inicio da criação de Notas, e adição de conteudo nos arquivos html

--- script/script.js
+++ script/script.js
@@ -4,3 +4,18 @@ const $btnMenuOpenAndClose = document.getElementById('btn-open-menu');  //cosnta
 $btnMenuOpenAndClose.addEventListener('click',function() {      //quando $btnMenuOpenAndClose for clicado a função sera executada
     $menu.classList.toggle("menuDiv-open");                     //a div $menu vai trocar de classe a cada clique;
 })
+
+//funcionamento dos botoes das notas
+var textarea = document.getElementById('textarea')
+var contentNote = [];
+//button salvar
+function salvar() {
+    contentNote = textarea.value;
+    console.log(contentNote)
+}
+
+
+//button excluir
+function reset() {
+    textarea.value = "";
+}
