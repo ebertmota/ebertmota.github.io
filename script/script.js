d9945e415370b3f09a6d7f076d7dfdf0ec73b1ee
ebertmota03 <ebert.mota03@gmail.com>
2019-07-17 10:50:32 -0300

    Notas integradas com localStorage adicionadas

--- script/script.js
+++ script/script.js
@@ -5,17 +5,3 @@ $btnMenuOpenAndClose.addEventListener('click',function() {      //quando $btnMen
     $menu.classList.toggle("menuDiv-open");                     //a div $menu vai trocar de classe a cada clique;
 })
 
-//funcionamento dos botoes das notas
-var textarea = document.getElementById('textarea')
-var contentNote = [];
-//button salvar
-function salvar() {
-    contentNote = textarea.value;
-    console.log(contentNote)
-}
-
-
-//button excluir
-function reset() {
-    textarea.value = "";
-}
