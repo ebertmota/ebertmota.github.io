c734f25f5a5bee9c76cc065378026a8a81887c4a
ebertmota <ebertmota03@gmail.com>
2019-08-01 10:13:36 -0300

    Estilizacao da function findString, e remorcao do google fonts

--- script/script.js
+++ script/script.js
@@ -5,57 +5,50 @@ $btnMenuOpenAndClose.addEventListener('click',function() {      //quando $btnMen
     $menu.classList.toggle("menuDiv-open");                     //a div $menu vai trocar de classe a cada clique;
 })
 
-document.addEventListener('keydown', (event) => {
-    const keyName = event.key;
-    console.log('key: '+ keyName);
-    console.log('keyCode: ' + event.which)
-}); 
-
 
     //AQUI ACONTECE A MAGICA 
-var TRange = null
-
-function findString (str)
- {
-  if (parseInt(navigator.appVersion) < 4) return
-
-  let strFound
-
-  if (window.find)
-   {
-    strFound = self.find(str)
-
-    if (!strFound)
-     {
-      strFound = self.find(str, 0, 1)
-      while (self.find(str, 0, 1)) continue
-     }
-   }
-
-  else if (navigator.appName.indexOf("Microsoft") != -1)
-
-   {
-    if (TRange != null)
-     {
-      TRange.collapse(false)
-      strFound = TRange.findText(str)
-      if (strFound) TRange.select()
-     }
-
-    if (TRange == null || strFound == 0)
-     {
-      TRange = self.document.body.createTextRange()
-      strFound=TRange.findText(str)
-      if (strFound) TRange.select()
-     }
-   }
-
-  else if (navigator.appName == "Opera")
-
-   {
-    return 'Opera não suportado'
-   }
-
-  if (!strFound) return ("Não foi encontrado '" + str)
-  return
- }
+    function findString (str)
+    {
+     let TRange = null
+     let strFound
+   
+     if (parseInt(navigator.appVersion) < 4) return
+   
+     if (window.find)
+      {
+       strFound = self.find(str)
+   
+       if (!strFound)
+        {
+         strFound = self.find(str, 0, 1)
+         while (self.find(str, 0, 1)) continue
+        }
+      }
+   
+     else if (navigator.appName.indexOf("Microsoft") != -1)
+   
+      {
+       if (TRange != null)
+        {
+         TRange.collapse(false)
+         strFound = TRange.findText(str)
+         if (strFound) TRange.select()
+        }
+   
+       if (TRange == null || strFound == 0)
+        {
+         TRange = self.document.body.createTextRange()
+         strFound = TRange.findText(str)
+         if (strFound) TRange.select()
+        }
+      }
+   
+     else if (navigator.appName == "Opera")
+   
+      {
+       return 'Opera não suportado'
+      }
+   
+     if (!strFound) return ("Não foi encontrado '" + str)
+     return
+    }
