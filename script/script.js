057111168ee8a0bc9a9b72fdf8f50e43b10e7564
ebertmota <ebertmota03@gmail.com>
2019-08-22 11:09:00 -0300

    adicao de conteudo e estilizacao geral

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
 //Abertura e fechamento do menu slide
 const $menu = document.querySelector('.menuDiv');  //const da div do menu slide
 const $btnMenuOpenAndClose = document.getElementById('btn-open-menu');  //cosntante do botão que abre e fecha a div
+const conteiner = document.getElementsByClassName('conteiner');
 $btnMenuOpenAndClose.addEventListener('click',function() {      //quando $btnMenuOpenAndClose for clicado a função sera executada
-    $menu.classList.toggle("menuDiv-open");                     //a div $menu vai trocar de classe a cada clique;
-})
-
+    $menu.classList.toggle("menuDiv-open");     //a div $menu vai trocar de classe a cada clique;              
+});
 
     //AQUI ACONTECE A MAGICA 
     function findString (str)
