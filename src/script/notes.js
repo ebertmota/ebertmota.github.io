841f89384da97c4db71b9b344592c4cc64a75095
ebertmota <ebertmota03@gmail.com>
2019-10-15 11:38:24 -0300

    created function addNote on html-notes

--- src/script/notes.js
+++ src/script/notes.js
@@ -1,106 +1,129 @@
-                    //NOTA 1
-//verificar se a nota foi salva
-if (localStorage.nota1) {
-    document.getElementById('textarea1').value = localStorage.nota1;
-}
-//botão salvar
-function salvarNota1() {
-    const textarea1 = document.getElementById('textarea1').value;
-    localStorage.setItem('nota1', textarea1);
-}
+const noteContainer = document.querySelector('.note-conteiner');
 
-//botão excluir
-function excluirNota1() {
-    localStorage.removeItem('nota1');
-    document.getElementById('textarea1').value = "";
-}
+//variavel usada no gerador de Id para cada textarea
+let id = 0
 
-                    //NOTA 2ex
-//verificar se a nota foi salva
-if (localStorage.nota2) {
-    document.getElementById('textarea2').value = localStorage.nota2;
-}
-//botão salvar
-function salvarNota2() {
-    const textarea2 = document.getElementById('textarea2').value;
-    localStorage.setItem('nota2', textarea2);
-}
-
-//botão excluir
-function excluirNota2() {
-    localStorage.removeItem('nota2');
-    document.getElementById('textarea2').value = "";
-}
+//procura suas notas salvas se não encontrar exibe um exemplo
+var noteStorage = JSON.parse(localStorage.getItem('notes'))  || ['Anote aqui...'];
 
 
-                    //NOTA 3 
-//verificar se a nota foi salva
-if (localStorage.nota3) {
-    document.getElementById('textarea3').value = localStorage.nota3;
+function renderNotes() {
+
+  
+  if (noteStorage == ""){
+    setTimeout( () => {
+      noteStorage = ["Dont live me alone baby...."];
+      renderNotes()
+    },4000)
+  }
+
+  //apaga todo conteudo para reconstruir
+  noteContainer.innerHTML = '';
+  
+  //para cada item de noteStorage...
+  for (notes of noteStorage){
+    newNote();
+    let textarea = document.getElementById(id);
+    textarea.value = notes;
+    
+  }
 }
 
-//botão salvar
-function salvarNota3(){
-    const textarea3 = document.getElementById('textarea3').value;
-    localStorage.setItem('nota3', textarea3)
-}
-function excluirNota3() {
-    localStorage.removeItem('nota3');
-    document.getElementById('textarea3').value = "";
+//assim que o documento carregar chama o renderNotes
+document.body.onload = renderNotes();
+
+function newNote() {
+  //chama o gerador de Id para cada Elemento ter uma primary Key
+  idGenerator();
+
+  //criaçao de elemntos/append childs 
+
+  let divElement = document.createElement('div');
+  divElement.setAttribute('class', 'note-box');
+  
+
+  let textareaElement = document.createElement('textarea');
+  textareaElement.setAttribute('id', id);
+  textareaElement.setAttribute('class', 'note-text');
+  
+
+  let divButton = document.createElement('div');
+  divButton.setAttribute('class', 'note-buttons')
+
+
+  let colorSwitch = document.createElement('button');
+  colorSwitch.setAttribute('class', 'submitButton');
+  let colorSwitchIcon = document.createElement('i');
+  colorSwitchIcon.setAttribute('class', 'fas fa-palette')
+  colorSwitch.appendChild(colorSwitchIcon)
+
+  
+  var pos = noteStorage.indexOf(String(notes));
+  
+
+  let saveBtn = document.createElement('button');
+  saveBtn.setAttribute('onclick', `saveNote(${pos})`);
+  saveBtn.setAttribute('class', 'submitButton');
+  let saveI = document.createElement('i');
+  saveI.setAttribute('class', 'far fa-save');
+  saveBtn.appendChild(saveI);
+
+
+  let deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('onclick', 'deleteNote()');
+  deleteBtn.setAttribute('class', 'submitButton');
+  let deleteI = document.createElement('i');
+  deleteI.setAttribute('class', 'far fa-trash-alt');
+  deleteBtn.appendChild(deleteI);
+  deleteBtn.setAttribute('onclick', `deleteNote(${pos})`);
+
+
+  noteContainer.appendChild(divElement);
+  divElement.appendChild(textareaElement)
+  divElement.appendChild(divButton);
+  divButton.appendChild(colorSwitch);
+  divButton.appendChild(saveBtn);
+  divButton.appendChild(deleteBtn);
+
 }
 
-                    //NOTA 4
+// quando o botao 'Adicionar Nota +' for clicado
 
-///verificar se a nota foi salva
-if (localStorage.nota4){
-    document.getElementById('textarea4').value = localStorage.nota4;
+function addNote() {
+  noteStorage.push('')
+  saveToStorage();
+  newNote();
+  renderNotes();
 }
 
-//salvar nota
-function salvarNota4() {
-    const textarea4 = document.getElementById('textarea4').value;
-    localStorage.setItem('nota4' , textarea4)
+
+function saveNote(pos) {
+  let textarea = document.getElementById(id);
+  //subistitue na posicao do elemento o valor antigo pelo atual EDITANDO a nota
+  noteStorage.splice(pos, 1,textarea.value);
+  renderNotes();
+  saveToStorage();
+
 }
 
-//excluir nota
-function excluirNota4() {
-    localStorage.removeItem('nota4')
-    document.getElementById('textarea4').value = "";
+function deleteNote(pos) {
+  //deleta a Nota atual do deleteBtn
+  noteStorage.splice(pos, 1);
+  renderNotes();
+  saveToStorage();
 }
 
-                    //NOTA 5
-
-//verificar se a nota foi salva
-if (localStorage.nota5) {
-    document.getElementById('textarea5').value = localStorage.nota5;
+function saveToStorage() {
+  //salva em string todo conteudo do array de notas
+  localStorage.setItem('notes', JSON.stringify(noteStorage));
 }
 
-//salvar a nota
-function salvarNota5() {
-    const textarea5 = document.getElementById('textarea5').value;
-    localStorage.setItem('nota5', textarea5);
-}
 
-//excluir a nota
-function excluirNota5() {
-    localStorage.removeItem('nota5');
-    document.getElementById('textarea5').value = "";
-}
-
-                    //NOTA 6
-//verificar se a nota foi salva
-if (localStorage.nota6){
-    document.getElementById('textarea6').value = localStorage.nota6;
-}
-
-//salvar a nota
-function salvarNota6() {
-    const textarea6 = document.getElementById('textarea6').value;
-    localStorage.setItem('nota6', textarea6);
-}
- 
-//excluir nota
-function excluirNota6() {
-    localStorage.removeItem('nota6');
-    document.getElementById('textarea6').value = "";
-}
+function idGenerator(){
+  let timeStamp = new Date();
+  id = timeStamp.getDate().toString() +
+       timeStamp.getHours().toString() +
+       timeStamp.getMinutes().toString() +
+       timeStamp.getSeconds().toString() +
+       timeStamp.getMilliseconds().toString();
+} 
